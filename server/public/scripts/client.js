c72a3465494575e39fd73c07f36abba3844f7076
Ken Dang <k3ndang@gmail.com>
2021-12-15 21:49:52 -0600

    when comment are bring submit, it append to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,14 +7,16 @@ $(document).ready(onReady);
 function onReady() {
     console.log('So damn ready');
 
-    $('button').on('click', onClick);
     $('#commentForm').on('submit', onAddComment);
 
-    // TODO
-    // write some code to get the data from the 
-    // GET /comments endpoint
-    // and then render that data to the DOM
+    // grab data from the server and render
+    // when the refresh button is clicked
+    $('#refresh').on('click', refresh);
 
+    // ..  and grab data and render
+    // immediately on page load, too.
+    refresh();
+};
 
     // Make a network request
     // Make a HTTP request
@@ -37,22 +39,29 @@ function onReady() {
         for that....
     `)
 
-};
+
 
 function onAddComment(event) {
+    // don't reload the page!
     event.preventDefault();
+
+    // prepare our message object
+    // to POST to the server
     let comment = {
         author:     $('#authorInput').val(),
         message:    $('#messageInput').val()
     }
-
+    console.log('comment', comment);
+    
 // send data to server
 $.ajax({
     method: 'POST',
     url:    '/comments',
     // send the comment to the server
     // in the request "body"
-    data:   comment
+    // this weill become req.body
+    // on the server
+    data: comment
 })
     .then((reponse) => {
         console.log('POST reponse', reponse);
@@ -63,27 +72,43 @@ $.ajax({
         refresh();
     })
 
-};    
+};
+
+function refresh() {
+    // make a network request
+    // make a HTTP request
+    // make an AJAX request
+    // AJAX === "Asynchronous Javascript and XML"
+    let ajaxOptions = {
+        method: 'GET',
+        url:    '/comments'
+    };
+    $.ajax(ajaxOptions)
+        .then((reponse) => {
+            console.log('AJAX request completed', reponse);
+            render(reponse);
+        });
+    console.log(`
+        Made a network request, but 
+        no one has time to wait 
+        for that....
+    `)
+    
+};
 
     // render function
     function render(comments) {
         // Do some jQuery to render comments (state) to the DOM
         // looping through the comments array
-
         $('#comments').empty();
-        for (let comment of comments)
+        for (let comment of comments) {
         $('#comments').append(`
             <li>
-             ${comment.message}!!!
+             ${comment.message}
                 <div>
-                - by ${comment.author}
+                    - by ${comment.author}
                 </div>
             </li>
         `)
     }
-
-
-
-    function onClick() {
-        $(this).css('background', 'green');
-    };
+}
--- server/server.js
+++ server/server.js
@@ -5,25 +5,22 @@
 
 // load express
 const express = require('express');
-
-// express is a function
-const app = express();
 const bodyParser = require('body-parser');
 
+
+// express is a function
+// that return an `app` object
+const app = express();
 
 // a shortcut to setup endpoints for
 // GET /index.html
 // GET /anotherone...
 app.use(express.static('server/public'));
 
-
-
-// don't fot body-parser!!
+// don't forget body-parser!!
 // https://i.imgur.com/qs0INo4.jpg
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-
 
 
 // setup a GET /space-jams endpoint
@@ -42,45 +39,38 @@ app.get('/space-jams', (req, res) => {
     `)
 });
 
-// GET /comments endpoint
-app.get('/comments', (req, res) => {
-    console.log('in GET /comments');
-    
-    res.send([
-        {
-            author:     'Ken',
-            message:    'New Space Jams sux, 1996 ftw'
-        }
-    ])
-
-});
-
 let comments = [ 
     {
-
-
+        author:     'Dang',
+        message:    'New Space Jams sux, 1996 ftw'
     }
 ];
+
+// GET /comments endpoint
+// localhost:5000/comments
+app.get('/comments', (req, res) => {
+    console.log('in GET /comments');
+    res.send(comments);
+});
 
 // POST /comments endpoint
 app.post('/comments', (req, res) => {
     console.log('in POST /comments', req.body);
 
     //TODO: save the comment to the server
-
+    comments.push(req.body)
 
     // send back a 👍
     res.sendStatus(201);
 });
 
 
-
 // listen on port 5000
 const port = 5000;
 app.listen(port, () => {
+    // this function is called when the 
+    // server is up and running
     console.log('I\'m listening 👂');
     
 });
 
-
-console.log('in server js 🧠');
